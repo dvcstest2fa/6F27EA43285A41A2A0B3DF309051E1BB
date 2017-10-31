96f2d999758c58a62d27c516708abc9ff6891b87
Kieran Senior <kieran_senior@apple.com>
2017-10-31 15:04:19 -0700

    Initial Commit

---
+++ 6F27EA43285A41A2A0B3DF309051E1BB/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  6F27EA43285A41A2A0B3DF309051E1BB
+//
+//  Created by Kieran Senior on 10/31/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
